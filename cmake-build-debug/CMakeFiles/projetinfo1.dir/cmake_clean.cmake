8515d4a4eb826e40f2b998017af6856335656b64
aurel <aurelien.kammerer@yahoo.com>
2024-11-02 12:44:00 +0100

    test 1

---
+++ cmake-build-debug/CMakeFiles/projetinfo1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projetinfo1.dir/main.c.obj"
+  "CMakeFiles/projetinfo1.dir/main.c.obj.d"
+  "libprojetinfo1.dll.a"
+  "projetinfo1.exe"
+  "projetinfo1.exe.manifest"
+  "projetinfo1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/projetinfo1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/projetinfo1.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for projetinfo1.
+# This may be replaced when dependencies are built.
