5158888eeccadf59d8b0fd105008210ab9b0201e
aurel <aurelien.kammerer@yahoo.com>
2024-11-02 13:30:29 +0100

    test 1 debut du menu debut du tableau

--- menu.c
+++ menu.c
@@ -2,6 +2,7 @@
 // Created by aurel on 02/11/2024.
 //
 #include <stdio.h>
+#define TAILLEPLATEAU 9
 
 int menudepart (char prenomjoueur1, char prenomjoueur2, char prenomjoueur3, char prenomjoueur4, int nombrejoueur) {
     printf("BIENVENUE DANS LE JEU DU QUORIDOR\n\n");
@@ -20,4 +21,18 @@ int menudepart (char prenomjoueur1, char prenomjoueur2, char prenomjoueur3, char
         scanf("%s", &prenomjoueur4);
     }
     return prenomjoueur1, prenomjoueur2, prenomjoueur3, prenomjoueur4, nombrejoueur;
+}
+
+int creationplateau(char plateau[TAILLEPLATEAU][TAILLEPLATEAU]) {
+    for (int i = 0; i < TAILLEPLATEAU; i++) {
+        for (int j = 0; j < TAILLEPLATEAU; j++) {
+            plateau[i][j] = '.';
+        }
+    }
+}
+
+int affichageplateau(char plateau[TAILLEPLATEAU][TAILLEPLATEAU]) {
+    printf(" A B C D E F G H I\n");
+    printf("________________________\n");
+    for (int i = 0; i < TAILLEPLATEAU; i++) {
 }
