e474cc7759eaf6fd8fe3eaa2b52825fcf25407ab
aurel <aurelien.kammerer@yahoo.com>
2024-11-16 14:33:58 +0100

    test 1 mouvement des pions debutplateau

--- menu.h
+++ menu.h
@@ -9,7 +9,9 @@
 int menudepart(char prenomjoueur1[], char prenomjoueur2[], char prenomjoueur3[],
     char prenomjoueur4[], int *nombrejoueur);
 void creationplateau(char plateau[TAILLEPLATEAU][TAILLEPLATEAU], int nombrejoueur);
-void affichageplateau(char plateau[TAILLEPLATEAU][TAILLEPLATEAU], char prenomjoueur1[],
-    char prenomjoueur2[], char prenomjoueur3[], char prenomjoueur4[], int *nombrejoueur);
+void affichageplateau(char plateau[TAILLEPLATEAU][TAILLEPLATEAU], char prenomjoueur1[], char prenomjoueur2[],
+    char prenomjoueur3[], char prenomjoueur4[], int *nombrejoueur, char lignepairecolonnepaire[TAILLECASEMAX],
+    char lignepairecolonneimpaire[TAILLECASEMAX], char ligneimpairecolonnepaire[TAILLECASEMAX],
+    char ligneimpairecolonneimpaire[TAILLECASEMAX]);
 
 #endif //MENU_H
