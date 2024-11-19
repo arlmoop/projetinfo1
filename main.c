62b2bba7613a242c1a083210f9a74e06935e9435
aurel <aurelien.kammerer@yahoo.com>
2024-11-19 15:02:12 +0100

    test 1 mouvement des pions debutplateau

--- main.c
+++ main.c
@@ -26,7 +26,7 @@ int main() {
     affichageplateau(plateau, prenomjoueur1, prenomjoueur2, prenomjoueur3, prenomjoueur4, &nombrejoueur);
 
     int joueurenmouvement =1; //variable comptant le joueur qui est en train de jouer
-    coordonnees coor_donnee;
+    coordonnees coordonnees_b;
     while (1) { /*Boucle gérant le tour des joueurs (pour l'instant infine car pas de code pour
         la fin de la partie*/
         do {
@@ -38,7 +38,7 @@ int main() {
                 affichageplateau(plateau, prenomjoueur1, prenomjoueur2, prenomjoueur3, prenomjoueur4, &nombrejoueur);
             }
             if (mouvement_barriere==2) {
-                pose_barriere(plateau, mouvement_barriere, coor_donnee);
+                pose_barriere(plateau, mouvement_barriere, coordonnees_b);
                 affichageplateau(plateau, prenomjoueur1, prenomjoueur2, prenomjoueur3, prenomjoueur4, &nombrejoueur);
             }
         } while (mouvement_barriere!=1 && mouvement_barriere!=2);
--- mouvementbarriere.c
+++ mouvementbarriere.c
@@ -9,38 +9,34 @@ typedef struct {
     char colonne;
 }coordonnees;
 
-coordonnees pose_barriere (char plateau[TAILLEPLATEAUVERTICALE][TAILLEPLATEAUHORIZONTALE], int mouvement_barriere, coordonnees coor_donnee) {
-    char coordonnee_barriere[4];
-    int v=0, h=0;
-    char lettre;
-    int nombre;
+coordonnees pose_barriere (char plateau[TAILLEPLATEAUVERTICALE][TAILLEPLATEAUHORIZONTALE], int mouvement_barriere, coordonnees coordonnees_b) {
     if (mouvement_barriere==2) {
         do {
             printf("Choisissez les coordonnées de l'emplacement de la barriere :");
             fflush(stdin);
-            scanf("%c%d", &coor_donnee.colonne, &coor_donnee.ligne);
-            printf("coord : %d %d\n", coor_donnee.colonne-'A', coor_donnee.ligne);
-            if((coor_donnee.ligne-1)%2==1) {
-                if((coor_donnee.colonne-'A')%2==0) {
-                    plateau[coor_donnee.ligne-1][(coor_donnee.colonne-'A')*3]='-';
-                    plateau[coor_donnee.ligne-1][(coor_donnee.colonne-'A')*3+1]='-';
-                    plateau[coor_donnee.ligne-1][(coor_donnee.colonne-'A')*3+2]='-';
+            scanf("%c%d", &coordonnees_b.colonne, &coordonnees_b.ligne);
+            printf("coord : %d %d\n", coordonnees_b.colonne-'A', coordonnees_b.ligne);
+            if((coordonnees_b.ligne-1)%2==1) {
+                if((coordonnees_b.colonne-'A')%2==0) {
+                    plateau[coordonnees_b.ligne-1][(coordonnees_b.colonne-'A')*3]='-';
+                    plateau[coordonnees_b.ligne-1][(coordonnees_b.colonne-'A')*3+1]='-';
+                    plateau[coordonnees_b.ligne-1][(coordonnees_b.colonne-'A')*3+2]='-';
                 }
-                else if((coor_donnee.colonne-'A')%2==1) {
+                else if((coordonnees_b.colonne-'A')%2==1) {
                     printf("Impossible\n");
                 }
             }
-            else if((coor_donnee.ligne-1)%2==0) {
-                if((coor_donnee.colonne-'A')%2==0) {
+            else if((coordonnees_b.ligne-1)%2==0) {
+                if((coordonnees_b.colonne-'A')%2==0) {
                     printf("Impossible\n");
                 }
-                else if((coor_donnee.colonne-'A')%2==1) {
-                    plateau[coor_donnee.ligne-1][(coor_donnee.colonne-'A')*3+1]='|';
+                else if((coordonnees_b.colonne-'A')%2==1) {
+                    plateau[coordonnees_b.ligne-1][(coordonnees_b.colonne-'A')*3+1]='|';
                 }
             }
-        } while ( (coor_donnee.ligne-1)%2==1 && (coor_donnee.colonne-'A')%2==1
-            || (coor_donnee.ligne-1)%2==0 && (coor_donnee.colonne-'A')%2==0
-            || coor_donnee.ligne<1 || coor_donnee.ligne>17 || coor_donnee.colonne<'A' || coor_donnee.colonne>'Q');
+        } while ( (coordonnees_b.ligne-1)%2==1 && (coordonnees_b.colonne-'A')%2==1
+            || (coordonnees_b.ligne-1)%2==0 && (coordonnees_b.colonne-'A')%2==0
+            || coordonnees_b.ligne<1 || coordonnees_b.ligne>17 || coordonnees_b.colonne<'A' || coordonnees_b.colonne>'Q');
     }
-    return coor_donnee;
+    return coordonnees_b;
 }
