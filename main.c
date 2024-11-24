d9646766fc6d0f80b853598559d4fc6fa0550414
IsiahTrombonne <isiahperelman13@gmail.com>
2024-11-24 09:38:35 +0100

    Update main.c

--- main.c
+++ main.c
@@ -14,12 +14,13 @@ int main() {
     //creation des variables pour le prenom des joueurs
     char prenomjoueur1[20], prenomjoueur2[20], prenomjoueur3[20], prenomjoueur4[20];
     int nombrejoueur;
+    char av1, av2, av3, av4;
     int mouvement_barriere, tourjoueur ;
     char plateau[TAILLEPLATEAUVERTICALE][TAILLEPLATEAUHORIZONTALE];
     coordonnees coordonnees_b, coordonnees_b1, coordonnees_b2, pion, pion1, pion2, pion3, pion4;
 
     //execution de la fonction qui affiche le menu
-    menudepart(prenomjoueur1, prenomjoueur2, prenomjoueur3, prenomjoueur4, &nombrejoueur);
+    menudepart(prenomjoueur1, prenomjoueur2, prenomjoueur3, prenomjoueur4, &nombrejoueur, &av1, &av2, &av3, &av4);
 
     if(nombrejoueur==2){
         tourjoueur=1+rand()%2;
@@ -28,16 +29,23 @@ int main() {
         tourjoueur=1+rand()%4;
     }
 
-    creationplateau(plateau, nombrejoueur, &pion1, &pion2, &pion3, &pion4 );
+    creationplateau(plateau, nombrejoueur, &pion1, &pion2, &pion3, &pion4, av1, av2, av3, av4);
     //pr verifier si c les bonnes coordonnees
-    printf("1: %d\n", pion1.ligne);
-    printf("1: %c\n", pion1.colonne);
-    printf("2: %d\n", pion2.ligne);
-    printf("2: %c\n", pion2.colonne);
-    printf("3: %d\n", pion3.ligne);
-    printf("3: %c\n", pion3.colonne);
-    printf("4: %d\n", pion4.ligne);
-    printf("4: %c\n", pion4.colonne);
+    printf("Coordoonnees :\n");
+    printf("1: %c", pion1.colonne);
+    printf(" %d\n", pion1.ligne);
+    printf("2: %c", pion2.colonne);
+    printf(" %d\n", pion2.ligne);
+    printf("3: %c", pion3.colonne);
+    printf(" %d\n", pion3.ligne);
+    printf("4: %c", pion4.colonne);
+    printf(" %d\n\n", pion4.ligne);
+    
+    printf("Pions :\n");
+    printf("1 : %c\n", av1);
+    printf("2 : %c\n", av2);
+    printf("3 : %c\n", av3);
+    printf("4 : %c\n", av4);
     
     affichageplateau(plateau, prenomjoueur1, prenomjoueur2, prenomjoueur3, prenomjoueur4, &nombrejoueur);
 
